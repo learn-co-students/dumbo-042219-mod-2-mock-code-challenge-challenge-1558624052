874819ccbbccbe270aa6353249ffd4c6d7e3df00
sebastosh <sebastosh@gmail.com>
2019-05-23 12:40:36 -0400

    failure is just opportunity for success

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,5 +2,43 @@ class EpisodesController < ApplicationController
 
   def index
     @episodes = Episode.all
+
   end
+
+  def new
+    @episode = Episode.new
+  end
+
+  def create
+    @episode = Episode.find(params[:id])
+
+    @episode = Episode.create(episode_params)
+
+    render :show
+  end
+
+  def show
+    @episode = Episode.find(params[:id])
+  end
+
+  def edit
+    @episode = Episode.find(params[:id])
+  end
+
+  def update
+    @episode = Episode.find(params[:id])
+
+    @episode = Episode.update(episode_params)
+
+    render :show
+  end
+
+private
+
+  def episode_params
+    params.require(:episode).permit(:date, :number)
+  end
+
+
+
 end
--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,42 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def new
+    @guest = Guest.new
+  end
+
+  def create
+    @guest = Guest.find(params[:id])
+
+    @guest = Guest.create(guest_params)
+
+    render :show
+  end
+
+  def show
+    @guest = Guest.find(params[:id])
+  end
+
+  def edit
+    @guest = Guest.find(params[:id])
+  end
+
+  def update
+    @guest = Guest.find(params[:id])
+
+    @guest = Guest.update(guest_params)
+
+    render :show
+  end
+
+private
+
+  def guest_params
+    params.require(:guest).permit(:name, :occupation, episode_ids: [])
+
+  end
+
+
+
 end
